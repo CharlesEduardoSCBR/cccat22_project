b3f5661803a75d043cbe9b4c869a72055663d067
Charles Eduardo <charles.eduardo.sc.br@gmail.com>
2025-10-20 11:08:42 +0000

    Automação de teste e TDD

---
+++ test/validateCpf.test.ts
@@ -0,0 +1,15 @@
+import { validateCpf } from "../src/validateCpf";
+
+test.each([
+  ["97456321558", true],
+  ["71428793860", true],
+  ["87748248800", true],
+  ["1", false],
+  ["", false],
+  ["11111111111", false],
+  ["00000000000", false],
+  ["00000000000564", false],
+])("Deve testar um cpf: %", (cpf, esperado) => {
+  const resultado = validateCpf(cpf);
+  expect(resultado).toBe(esperado);
+});
