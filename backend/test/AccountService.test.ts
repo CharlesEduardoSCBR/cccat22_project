9a0ab9c109930b6b5e6140249933dd36e4eb32cc
Charles Eduardo <charles.eduardo.sc.br@gmail.com>
2025-10-24 12:57:12 +0000

    Fim da implementação do tests patterns

--- backend/test/AccountService.test.ts
+++ backend/test/AccountService.test.ts
@@ -70,3 +70,46 @@ test("Deve criar uma conta com spy", async () => {
   saveSpy.restore();
   getAccountByIdSpy.restore();
 });
+
+test("Deve criar uma conta com mock", async () => {
+  const accountDAOMock = sinon.mock(AccountDAODatabase.prototype);
+  accountDAOMock.expects("save").once().resolves();
+  const account = {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    document: "97456321558",
+    password: "asdQWE123",
+  };
+  accountDAOMock.expects("getById").once().resolves(account);
+  const outputSignup = await accountService.signup(account);
+  const outputGetAccount = await accountService.getAccountById(outputSignup.accountId);
+  expect(outputGetAccount.accountId).toBeDefined();
+  expect(outputGetAccount.name).toBe(account.name);
+  expect(outputGetAccount.email).toBe(account.email);
+  expect(outputGetAccount.document).toBe(account.document);
+  expect(outputGetAccount.password).toBe(account.password);
+  accountDAOMock.verify();
+  accountDAOMock.restore();
+});
+
+test("Deve criar uma conta com fake", async () => {
+  const accountDAO = new AccountDAOMemory();
+  const accountService = new AccountService(accountDAO);
+  const account = {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    document: "97456321558",
+    password: "asdQWE123",
+  };
+
+  const outputSignup = await accountService.signup(account);
+  const outputGetAccount = await accountService.getAccountById(
+    outputSignup.accountId
+  );
+  console.log(outputGetAccount);
+  expect(outputGetAccount.accountId).toBeDefined();
+  expect(outputGetAccount.name).toBe(account.name);
+  expect(outputGetAccount.email).toBe(account.email);
+  expect(outputGetAccount.document).toBe(account.document);
+  expect(outputGetAccount.password).toBe(account.password);
+});
